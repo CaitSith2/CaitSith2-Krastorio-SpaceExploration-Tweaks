58e5e489ee01e9e1dbbe4b598b9bf9eeb2776dd2
CaitSith2 <d_good@caitsith2.com>
2022-02-17 15:56:24 -0800

    Remove the grounded factory buildings from space/spaceships. It was impossible to make grounded buildings work on spaceships without also allowing them in space unless I implement the check control.lua side.

--- tweaks/spaceship.lua
+++ tweaks/spaceship.lua
@@ -1,34 +1,6 @@
 
 
 if mods["space-exploration"] then
-	if mods["Factorissimo2"] then -- Keeping this code here for commit history purposes. Not going to actually allow grounded factory buildings on spaceships at present.
-		if settings.startup["cs2-tweaks-allow-factory-buildings-on-spaceship"].value == true then
-			collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["factory-1"].collision_mask, space_collision_layer)
-			collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["factory-1"].collision_mask, spaceship_collision_layer)
-			collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["factory-2"].collision_mask, space_collision_layer)
-			collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["factory-2"].collision_mask, spaceship_collision_layer)
-			collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["factory-3"].collision_mask, space_collision_layer)
-			collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["factory-3"].collision_mask, spaceship_collision_layer)
-			collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["factory-1-i"].collision_mask, space_collision_layer)
-			collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["factory-1-i"].collision_mask, spaceship_collision_layer)
-			collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["factory-2-i"].collision_mask, space_collision_layer)
-			collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["factory-2-i"].collision_mask, spaceship_collision_layer)
-			collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["factory-3-i"].collision_mask, space_collision_layer)
-			collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["factory-3-i"].collision_mask, spaceship_collision_layer)
-			for i = 10, 99 do
-				collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["factory-1-s" .. i].collision_mask, space_collision_layer)
-				collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["factory-1-s" .. i].collision_mask, spaceship_collision_layer)
-				collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["factory-2-s" .. i].collision_mask, space_collision_layer)
-				collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["factory-2-s" .. i].collision_mask, spaceship_collision_layer)
-				collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["factory-3-s" .. i].collision_mask, space_collision_layer)
-				collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["factory-3-s" .. i].collision_mask, spaceship_collision_layer)
-			end
-			collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["factory-input-pipe"].collision_mask, space_collision_layer)
-			collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["factory-output-pipe"].collision_mask, space_collision_layer)
-			collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["factory-input-pipe"].collision_mask, spaceship_collision_layer)
-			collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["factory-output-pipe"].collision_mask, spaceship_collision_layer)
-		end
-	end
 	if mods["space-factorissimo-updated"] then
 		if settings.startup["cs2-tweaks-allow-factory-buildings-on-spaceship"].value == true then
 			collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["space-gravFactory-3"].collision_mask, spaceship_collision_layer)
@@ -39,14 +11,14 @@ if mods["space-exploration"] then
 				collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["space-gravFactory-3-s" .. i].collision_mask, spaceship_collision_layer)
 				collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["space-factory-3-s" .. i].collision_mask, spaceship_collision_layer)
 			end
-			if data.raw["storage-tank"]["space-factory-4"] then
+			if data.raw["storage-tank"]["space-factory-4"] then -- Space factorissimo doesn't currently enable space-factory-4 nor space-factory-5
 				collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["space-factory-4"].collision_mask, spaceship_collision_layer)
 				collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["space-factory-4-i"].collision_mask, spaceship_collision_layer)
 				for i = 10,99 do
 					collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["space-factory-4-s" .. i].collision_mask, spaceship_collision_layer)
 				end
 			end
-			if data.raw["storage-tank"]["space-factory-5"] then
+			if data.raw["storage-tank"]["space-factory-5"] then -- this code is however here just in case it gets enabled in the future.
 				collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["space-factory-5"].collision_mask, spaceship_collision_layer)
 				collision_mask_util_extended.remove_layer(data.raw["storage-tank"]["space-factory-5-i"].collision_mask, spaceship_collision_layer)
 				for i = 10,99 do
